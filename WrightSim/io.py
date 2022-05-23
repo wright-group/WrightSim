aa9a5636525d0155f8772d7642786ac1b5c988d9
kameyer226 <kentmeyer@wisc.edu>
2022-05-23 13:17:51 -0500

    added io python file

---
+++ WrightSim/io.py
@@ -0,0 +1,50 @@
+import WrightTools as wt
+import numpy as np
+import json
+
+def to_wtdata(wsexp, wsrun):
+    '''
+    to_wtdata(wsexp, wsrun)
+    -----------
+    Converts a WrightSim experiment and run into a properly formatted WrightTools data object.
+
+    Inputs
+    -- 
+    wsexp:  WrightSim experiment object.
+    wsrun:  WrightSim run object.
+
+
+    Output
+    ----
+    data:  WrightTools data object.
+
+    '''
+    return
+
+
+def save_exp(filename, wsexp):
+    '''
+    Saves a WrightSim experiment to a JSON file.
+    '''
+    return
+
+
+def load_exp(filename):
+    '''
+    Loads a JSON file into a WrightSim object.
+    '''
+    return
+
+
+def save_run(ht5file, wsrun):
+    '''
+    Saves a WrightSim run result to an hdf5 file.
+    '''
+    return
+
+
+def load_run(ht5file):
+    '''
+    Loads an hdf5 file into a WrightSim run object.
+    '''
+    return
