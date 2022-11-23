6adccda6bea8ffd79eb70aeb3273e6e34e0c8ef4
pcruzparri <peter.cruzparrilla@gmail.com>
2022-11-22 22:45:27 -0600

    _transientsv3 is now usable, it is a class calculating spectral outputs. scriptv3 uses _transientsv3 for scanning frequencies. Next step will be delay scans.

--- transients/_scan_2dfreq.py
+++ transients/_scan_2dfreq.py
@@ -7,7 +7,7 @@ import time
 
 
 class TransientOut():
-    def __init__(self, omegas, gammas, rabis, delays=[],pws=[],times=[], pulse_freqs=[]):
+    def __init__(self, omegas, gammas, rabis, delays=[], pws=[], times=[], pulse_freqs=[]):
         self.omegas = list(map(_trans.wntohz, omegas))
         self.gammas = gammas
         self.rabis = rabis
@@ -44,41 +44,42 @@ class TransientOut():
         return self.times
 
     def set_pulse_freqs(self, freqs):
-        self.pulse_freqs = map(_trans.wntohz, freqs)
+        self.pulse_freqs = list(map(_trans.wntohz, freqs))
 
     def get_pulse_freqs(self):
         return self.pulse_freqs
 
-    def dove_ir_1_scan(self,scan_freqs, npts, time_int=1000):
+    def dove_ir_1_freq_scan(self, scan_freqs, npts, time_int=1000):
         # scan parameters
         w1_center = scan_freqs[0]
         w2_center = scan_freqs[1]
         w1_range = scan_freqs[2]
         w2_range = scan_freqs[3]
 
-        scan_npts = npts
-        w1_scan_range = np.linspace(w1_center-w1_range, w1_center+w1_range, scan_npts)
-        w2_scan_range = np.linspace(w2_center-w2_range, w2_center+w2_range, scan_npts)
+        w1_scan_range = np.linspace(w1_center-w1_range, w1_center+w1_range, npts[0])
+        w2_scan_range = np.linspace(w2_center-w2_range, w2_center+w2_range, npts[1])
 
         scan = np.zeros((len(w1_scan_range), len(w2_scan_range)))
 
         scan_start = time.time()
         remaining = len(w1_scan_range)*len(w2_scan_range)
+        last_speed = [0]*5
 
         for ind2, w2 in enumerate(w2_scan_range):  # y axis
             for ind1, w1 in enumerate(w1_scan_range):  # x axis
-                self.pulse_freqs[0], self.pulse_freqs[1] = w1, w2
+                self.set_pulse_freqs([w1, w2, self.omegas[2]])
+
                 time1 = time.time()
 
                 T1 = _trans.bra_abs(self.rabis[0],
-                                        _trans.delta_ij(0, 1/1e-12),
+                                        _trans.delta_ij(0, 1e-12),
                                         _trans.delta_ij(self.omegas[0], self.gammas[0]),
                                         self.pulse_freqs[0],
                                         self.omegas[0],
                                         self.gammas[0],
-                                        1/1e-12,
-                                        self.times[0])  # trans1, driven, 0 to t1
-                FID1 = _trans.fid(1, _trans.delta_ij(self.omegas[0], self.gammas[0]), self.times[2]-self.times[0])
+                                        1e-12,
+                                        self.times[1])  # trans1, driven, 0 to t1
+                FID1 = _trans.fid(1, _trans.delta_ij(self.omegas[0], self.gammas[0]), self.times[3]-self.times[1])
                 T2 = _trans.ket_abs(self.rabis[1],
                                         _trans.delta_ij(self.omegas[0], self.gammas[0]),
                                         _trans.delta_ij(self.omegas[1], self.gammas[1]),
@@ -86,11 +87,11 @@ class TransientOut():
                                         self.omegas[1],
                                         self.gammas[1],
                                         self.gammas[0],
-                                        self.times[2]-self.times[1])
+                                        self.times[3]-self.times[2])
                 FID2 = _trans.fid(1, _trans.delta_ij(self.omegas[1], self.gammas[1]),
-                                        np.linspace(self.times[3]-self.times[2], \
-                                            self.times[4]-self.times[2], \
-                                                int((self.times[4]-self.times[3])*1e15*time_int)+1))
+                                        np.linspace(self.times[4]-self.times[3], \
+                                            self.times[5]-self.times[3], \
+                                                int((self.times[5]-self.times[4])*1e15*time_int)+1))
                 T3 = _trans.ket_abs(self.rabis[2],
                                         _trans.delta_ij(self.omegas[1], self.gammas[1]),
                                         _trans.delta_ij(self.omegas[2], self.gammas[2]),
@@ -98,7 +99,7 @@ class TransientOut():
                                         self.omegas[2],
                                         self.gammas[2],
                                         self.gammas[1],
-                                        np.linspace(0, self.times[4]-self.times[3], int((self.times[4]-self.times[3])*1e15*time_int)+1))
+                                        np.linspace(0, self.times[5]-self.times[4], int((self.times[5]-self.times[4])*1e15*time_int)+1))
 
                 coeff = T1*FID1*T2
                 test = np.array(coeff*FID2*T3)
@@ -107,22 +108,20 @@ class TransientOut():
                 remaining -= 1
 
                 time2 = time.time()
+                round_time = round(time2-time1, 2)
+                last_speed = last_speed[1:]+[round_time]
                 print(f'Finished x={w1} and y={w2} | '
-                    f'Calc. time was {round(time2-time1,2)} s | '
-                    f'Time remaining is {round((time2-time1)*remaining/60, 2)} min')
+                    f'Calc. time was {round_time} s | '
+                    f'Time remaining is {round(np.average(last_speed)*remaining/60, 2)} min')
         scan_end = time.time()
         print(f'Total calc. time was {scan_end-scan_start}s')
-        # print(scan)
 
 
         #scan = gaussian_filter(scan, sigma=2)
-        im = plt.imshow(scan, cmap='bwr', origin='lower', extent=(min(w1_scan_range),
+        plt.imshow(scan, cmap='bwr', origin='lower', extent=(min(w1_scan_range),
                                                             max(w1_scan_range),
                                                             min(w2_scan_range),
                                                             max(w2_scan_range)))
-
         plt.show()
 
 
-
-    
--- transients/script_v3.py
+++ transients/script_v3.py
@@ -1,9 +1,8 @@
 from _scan_2dfreq import *
+import _transientsv3 as _trans
 
-
-
-omegas = [1600, 3100, 9800]
-gammas = [2e12, 3e12, 20e12]
+omegas = [2254, 3165, 9800]
+gammas = list(map(_trans.wntohz, (3.9, 4.7, 0)))
 rabis = [2.045e12, 5.817e11, 9.933e12]
 exp = TransientOut(omegas, gammas, rabis)
 
@@ -18,8 +17,10 @@ pw3 = 600e-15
 pws = [pw1, pw2, pw3]
 exp.set_pws(pws)
 exp.set_times()
+exp.set_pulse_freqs([0, 0, omegas[2]])
 
-exp.dove_ir_1_scan([1600,3100,200,200], 21)
+
+exp.dove_ir_1_freq_scan([omegas[0], omegas[1], 50, 50], [11, 11])
 
 
 
